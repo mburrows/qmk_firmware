fd33af50a312ef25a9703d2197f6ff538acb4c8f
Matt Burrows <m@ttburro.ws>
2019-05-31 12:08:20 +0100

    Added leader key definitions

--- keyboards/handwired/dactyl_manuform/4x5_transposed/keymaps/mburrows/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5_transposed/keymaps/mburrows/keymap.c
@@ -115,6 +115,174 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
+    eeconfig_update_default_layer(default_layer);
+    default_layer_set(default_layer);
+}
+
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+    LEADER_DICTIONARY() {
+        leading = false;
+        leader_end();
+
+        // Single key macros
+        SEQ_ONE_KEY(KC_2) {
+            SEND_STRING (" 2>&1");
+        }
+        SEQ_ONE_KEY(KC_F) {
+            SEND_STRING("find . -name ");
+        }
+        SEQ_ONE_KEY(KC_G) {
+            SEND_STRING("grep -E ''"SS_TAP(X_LEFT));
+        }
+        SEQ_ONE_KEY(KC_M) {
+            SEND_STRING("mburrows");
+        }
+        SEQ_ONE_KEY(KC_N) {
+            SEND_STRING (" > /dev/null");
+        }
+        SEQ_ONE_KEY(KC_S) {
+            SEND_STRING("\n\nRegards\nMatt\n");
+        }
+        SEQ_ONE_KEY(KC_T) {
+            SEND_STRING("tail -F ");
+        }
+        SEQ_ONE_KEY(KC_SCLN) {
+            SEND_STRING(SS_TAP(X_END)";");
+        }
+        SEQ_ONE_KEY(KC_C) {
+            // shortcut for tmux copy mode
+            SEND_STRING(SS_LCTRL(" ")"[");
+        }
+        SEQ_ONE_KEY(KC_V) {
+            // shortcut for tmux paste mode
+            SEND_STRING(SS_LCTRL(" ")"]");
+        }
+
+        // Pipe macros
+        SEQ_TWO_KEYS(KC_P, KC_G) {
+            SEND_STRING(" | grep -E ''"SS_TAP(X_LEFT));
+        }
+        SEQ_TWO_KEYS(KC_P, KC_L) {
+            SEND_STRING(" | less");
+        }
+        SEQ_TWO_KEYS(KC_P, KC_H) {
+            SEND_STRING(" | head");
+        }
+        SEQ_TWO_KEYS(KC_P, KC_T) {
+            SEND_STRING(" | tail");
+        }
+
+        // Print macros
+        SEQ_TWO_KEYS(KC_P, KC_W) {
+            SEND_STRING("prt_wire ");
+        }
+        SEQ_TWO_KEYS(KC_P, KC_F) {
+            SEND_STRING("prt_fix --nohb ");
+        }
+        SEQ_TWO_KEYS(KC_P, KC_M) {
+            SEND_STRING("prt_mktdata ");
+        }
+
+        // Email macros
+        SEQ_TWO_KEYS(KC_E, KC_H) {
+            SEND_STRING("maburrow@googlemail.com");
+        }
+        SEQ_TWO_KEYS(KC_E, KC_G) {
+            SEND_STRING("m@ttburro.ws");
+        }
+        SEQ_TWO_KEYS(KC_E, KC_W) {
+            SEND_STRING("mburrows@cboe.com");
+        }
+
+        // Git macros
+        SEQ_TWO_KEYS(KC_G, KC_S) {
+            SEND_STRING("git status -sb");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_L) {
+            SEND_STRING("git log");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_C) {
+            SEND_STRING("git commit");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_P) {
+            SEND_STRING("git push");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_F) {
+            SEND_STRING("git pull");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_D) {
+            SEND_STRING("git diff -w");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_1) {
+            SEND_STRING("git log --oneline");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_B) {
+            SEND_STRING("git branch ");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_V) {
+            SEND_STRING("git svnup");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_G) {
+            SEND_STRING("git grep -n --break ''"SS_TAP(X_LEFT));
+        }
+        SEQ_TWO_KEYS(KC_G, KC_U) {
+            SEND_STRING("git add -u");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_M) {
+            SEND_STRING("git log --author=mburrows");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_R) {
+            SEND_STRING("git rebase ");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_O) {
+            SEND_STRING("git checkout ");
+        }
+        SEQ_TWO_KEYS(KC_G, KC_Y) {
+            SEND_STRING("git commit --amend -C HEAD ");
+        }
+
+        // Wire macros
+        SEQ_TWO_KEYS(KC_W, KC_T) {
+            SEND_STRING("wtail ");
+        }
+        SEQ_TWO_KEYS(KC_W, KC_H) {
+            SEND_STRING("whead ");
+        }
+        SEQ_TWO_KEYS(KC_W, KC_G) {
+            SEND_STRING("wgrep --line-buffered -P ");
+        }
+        SEQ_TWO_KEYS(KC_W, KC_R) {
+            SEND_STRING("wrange -r");
+        }
+        SEQ_TWO_KEYS(KC_W, KC_S) {
+            SEND_STRING("wgrep --survey ");
+        }
+
+        // Jira macros
+        SEQ_TWO_KEYS(KC_J, KC_I) {
+            SEND_STRING("{issue:EUCPP-}"SS_TAP(X_LEFT));
+        }
+        SEQ_TWO_KEYS(KC_J, KC_N) {
+            SEND_STRING ("{noformat}");
+        }
+        SEQ_TWO_KEYS(KC_J, KC_B) {
+            SEND_STRING ("https://jira.bats.com/browse/EUCPP-");
+        }
+
+        // SQL macros
+        SEQ_TWO_KEYS(KC_S, KC_S) {
+            SEND_STRING ("select * from ");
+        }
+        SEQ_TWO_KEYS(KC_S, KC_P) {
+            SEND_STRING ("select * from proc_param('');" SS_TAP(X_LEFT) SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+        }
+        SEQ_TWO_KEYS(KC_S, KC_D) {
+            SEND_STRING ("select * from symbol_daily where effective_date=current_date and symbol_name='';" SS_TAP(X_LEFT) SS_TAP(X_LEFT));
+        }
+        SEQ_TWO_KEYS(KC_S, KC_R) {
+            SEND_STRING ("current_date between begin_dt and end_dt");
+        }
+    }
 }
